97e67b5805149b1194a7f724fac3bd8ad375b9fb
InduShashi <shashiadiveppa44@gmail.com>
2024-07-20 18:23:20 +0530

    removed cdm file

--- cdm_pet_profile_export_into_iceberg_inc2.sql
+++
@@ -1,8 +0,0 @@
-copy into @admin.{{env_edl}}_chewy_bi_iceberg_data_export_stage/pet_profile/{{uuid}}/
-from 
-(select 
-* from
-cdm.pet_profile where TIME_CREATED :: date >= current_date - {{process_days}}
-) 
--- partition by TIME_CREATED :: date :: string
-  header = true;
